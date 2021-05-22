6ddd824da4927c696c3f5d0eeb9e5d213497aba0
Nick Browne <nick.squdge@outlook.com>
2021-05-22 19:27:45 +0100

    Project setup

---
+++ src/DotnetGenerate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace DotnetGenerate
+{
+    public class Program
+    {
+        private readonly IConsole _console;
+
+        public Program(IConsole console)
+        {
+            if (console == null)
+                throw new ArgumentNullException();
+
+            _console = console;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
