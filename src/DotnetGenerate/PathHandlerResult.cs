80d418c57f7541224c22371693116fcfdf8f3c3f
Nick Browne <nick.squdge@outlook.com>
2021-06-06 19:53:55 +0100

    Added the open command. started to add in the interface and class schematic

---
+++ src/DotnetGenerate/PathHandlerResult.cs
@@ -0,0 +1,10 @@
+namespace DotnetGenerate
+{
+    public class PathHandlerResult
+    {
+        public string RelativePath { get; set; }
+        public string FullPath { get; set; }
+        public string Namespace { get; set; }
+        public string OriginalFileName { get; set; }
+    }
+}
